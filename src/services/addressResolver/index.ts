37a0c1c9a71c142629b4e875275424752b67ae43
justussoh <justus.soh@hotmail.com>
2020-08-06 15:58:39 +0800

    feat: added axios caching for address endpoint resolver

--- src/services/addressResolver/index.ts
+++ src/services/addressResolver/index.ts
@@ -1,12 +1,18 @@
-import axios from "axios";
+import axios, { AxiosAdapter } from "axios";
 import { ThirdPartyAPIEntryProps } from "../../common/hooks/useThirdPartyAPIEndpoints";
 import { getLogger } from "./../../utils/logger";
+import { cacheAdapterEnhancer } from "axios-extensions";
 
 const { trace } = getLogger("service:addressresolver");
 
 export const resolveAddressNameByEndpoint = async (url: string) => {
+  // Default TTL is 5 Mins to change timeout check https://github.com/kuitos/axios-extensions#cacheadapterenhancer
+  const client = axios.create({
+    headers: { "Cache-Control": "no-cache" },
+    adapter: cacheAdapterEnhancer(axios.defaults.adapter as AxiosAdapter), // Typecast suggested by author to force non-null typing: https://github.com/kuitos/axios-extensions/issues/8
+  });
   try {
-    const response = await axios.get(url);
+    const response = await client.get(url);
     return response.data?.identity?.name;
   } catch (e) {
     trace(`Resolve Address Status: ${e}`);
